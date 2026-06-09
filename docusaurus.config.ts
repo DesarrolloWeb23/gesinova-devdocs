47520c371fbad6136c4de37a1ec0ddf150676c6a
DesarrolloWeb23 <Desarrollo1@pijaossalud.com.co>
2026-06-09 08:03:26 -0500

    first commit

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,8 +5,8 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Gesinova Devdocs',
+  tagline: 'Documentacion tecnica e informacion de procesos tecnicos',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
@@ -75,9 +75,9 @@ const config: Config = {
       respectPrefersColorScheme: true,
     },
     navbar: {
-      title: 'My Site',
+      title: 'Gesinova Devdocs',
       logo: {
-        alt: 'My Site Logo',
+        alt: 'GesinovaLogo',
         src: 'img/logo.svg',
       },
       items: [
